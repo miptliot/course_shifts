2162aee3db722734fa3b37d27e15e09466fd20fe
Boris Zimka <zimka.b@ya.ru>
2018-02-17 10:14:38 +0000

    Add migration for Ginkgo

--- course_shifts/migrations/0001_initial.py
+++ course_shifts/migrations/0001_initial.py
@@ -5,14 +5,14 @@ from django.db import migrations, models
 import course_shifts.models
 from django.conf import settings
 import django.core.validators
-import xmodule_django.models
+import openedx.core.djangoapps.xmodule_django.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('course_groups', '0002_auto_20171110_0815'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('course_groups', '0004_auto_20180216_0701'),
     ]
 
     operations = [
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
             name='CourseShiftGroup',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('course_key', xmodule_django.models.CourseKeyField(help_text=b'Which course is this group associated with', max_length=255, db_index=True)),
+                ('course_key', openedx.core.djangoapps.xmodule_django.models.CourseKeyField(help_text=b'Which course is this group associated with', max_length=255, db_index=True)),
                 ('start_date', models.DateField(default=course_shifts.models.date_now, help_text=b'Date when this shift starts')),
                 ('days_shift', models.IntegerField(default=0, help_text=b"Days to add to the block's due")),
                 ('course_user_group', models.OneToOneField(to='course_groups.CourseUserGroup')),
@@ -38,7 +38,7 @@ class Migration(migrations.Migration):
             name='CourseShiftSettings',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('course_key', xmodule_django.models.CourseKeyField(unique=True, max_length=255, db_index=True)),
+                ('course_key', openedx.core.djangoapps.xmodule_django.models.CourseKeyField(unique=True, max_length=255, db_index=True)),
                 ('is_shift_enabled', models.BooleanField(default=False, help_text=b'True value if this feature is enabled for the course run')),
                 ('is_autostart', models.BooleanField(default=True, help_text=b'Are groups generated automatically with period or according to the m  anually set plan')),
                 ('autostart_period_days', models.PositiveIntegerField(default=28, help_text=b'Number of days between new automatically generated shifts.Used only in autostart mode.', null=True, db_column=b'autostart_period_days', validators=[django.core.validators.MinValueValidator(0)])),
